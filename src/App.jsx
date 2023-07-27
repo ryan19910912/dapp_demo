f1a89df403df85ca602e0d67edb21e033df35070
Ryan Hsu <ryan.hsu@hytechc.com>
2023-07-27 18:20:30 +0800

    modify App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { ConnectButton, useWallet } from '@suiet/wallet-kit'
-import { JsonRpcProvider, Connection, devnetConnection, testnetConnection, mainnetConnection } from '@mysten/sui.js';
+import { JsonRpcProvider, devnetConnection, testnetConnection, mainnetConnection } from '@mysten/sui.js';
 
 function App() {
 
   const wallet = useWallet()
 
-  // const devRpcUrl = "https://fullnode.devtnet.sui.io";
-  // const testRpcUrl = "https://fullnode.testnet.sui.io";
-  // const mainRpcUrl = "https://fullnode.mainnet.sui.io";
+  /**
+   * sui fullnode rpc url
+   */
+  const devRpcUrl = "https://fullnode.devtnet.sui.io";
+  const testRpcUrl = "https://fullnode.testnet.sui.io";
+  const mainRpcUrl = "https://fullnode.mainnet.sui.io";
 
   // connect to Testnet
   const provider = new JsonRpcProvider(devnetConnection);
+
+  const jsonFormat = (str) => {
+    return JSON.stringify(str, null, 2);
+  }
 
   provider.getOwnedObjects({
     owner: "0x7e37a75bde4fbb88dd3c815bf892fe04369b9900481ee0d60747d9ba77ad215d",
@@ -24,7 +31,7 @@ function App() {
       showContent: true,
     }
   }).then(data => {
-    console.log("data = " + JSON.stringify(data));
+    console.log("data = " + jsonFormat(data));
   });
 
   return (
